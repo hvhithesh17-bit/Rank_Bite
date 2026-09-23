1c1b08c33a36607bc0488e3c2d95b24579628efa
HITHESH\hvhit <hvhithesh17@gmail.com>
2026-09-23 18:55:09 +0530

    refactor: integrate project modules

---
+++ src/main/java/com/rankbite/Main.java
@@ -0,0 +1,18 @@
+package com.rankbite;
+
+import com.rankbite.gui.LoginFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Set native look and feel
+                javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
